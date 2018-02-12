7edaef3432db65e2ac5eb1a48d3cdf8f3ab5fe25
JS <wjstanek@gmail.com>
2018-02-12 12:23:56 -0700

    jeff

--- gui.py
+++ gui.py
@@ -1,5 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QGridLayout,
+    QLCDNumber, QVBoxLayout)
 
 class Example(QWidget):
     def __init__(self):
@@ -9,14 +11,36 @@ class Example(QWidget):
         self.resize(500,500)
         self.setWindowTitle('KSP GUI')
 
-        label = QLabel('Speed: ' + str(speed))
+        label_Speed = QLabel('Speed: ')
+        label_Dist = QLabel('Dist: ')
+        label_Alt = QLabel('Alt:  ')
+        speed = 5
+        varSpeed = QLCDNumber()
+        varSpeed.display(speed)
 
+        grid = QGridLayout()
+        grid.setSpacing(10)
+        grid.addWidget(label_Speed,1,0)
+        grid.addWidget(label_Dist,2,0)
+        grid.addWidget(label_Alt,3,0)
+        grid.addWidget(varSpeed,1,1)
+
+        self.setLayout(grid)
         self.show()
 
-def main(sensorData):
+    def keyPressEvent(self,e):
+        if e.key() == Qt.Key_Escape:
+            self.close()
+        if e.key() == Qt.Key_Tab:
+            speed = 10
+            #self.grid.addWidget(varSpeed,2,1)
+            processEvents()
+
+def main():
     app = QApplication(sys.argv)
     ex = Example()
     sys.exit(app.exec_())
 
+
 if __name__ == '__main__':
-    pass
+    main()
